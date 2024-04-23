ab2066e48d2070fbab4ed64f8c4796963f45533e
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-04-23 23:00:41 +0300

    Add Game interface to db.d.ts

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -4,3 +4,11 @@ interface User {
   email: string;
   image: string;
 }
+
+interface Game {
+  id: string;
+  players: string[];
+  status: "pending" | "playing" | "finished";
+  board_0: string[][];
+  board_1: string[][];
+}
