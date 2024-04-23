1809b12d5fceb5667e905dc4047ecc0dc5163d1c
Furkan Gündüz <furkan.gundz00@gmail.com>
2024-04-23 23:00:19 +0300

    Update PusherEvents constants to use double quotes instead of single quotes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,12 @@
-import { type ClassValue, clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
 export function toPusherKey(key: string) {
-  return key.replace(/:/g, '__');
+  return key.replace(/:/g, "__");
 }
 
 export function chatHrefConstructor(id1: string, id2: string) {
@@ -16,19 +16,24 @@ export function chatHrefConstructor(id1: string, id2: string) {
 
 export const PusherEvents = {
   REQUESTS: {
-    INCOMING: 'incoming_friend_requests',
-    OUTGOING: 'outgoing_friend_requests',
-    DENY: 'deny_request',
-    ACCEPT: 'accept_request',
-    CANCEL: 'cancel_request',
+    INCOMING: "incoming_friend_requests",
+    OUTGOING: "outgoing_friend_requests",
+    DENY: "deny_request",
+    ACCEPT: "accept_request",
+    CANCEL: "cancel_request",
   },
   FRIENDS: {
-    NEW: 'new_friend',
-    REMOVE: 'remove_friend',
+    NEW: "new_friend",
+    REMOVE: "remove_friend",
   },
   MESSAGES: {
-    INCOMING: 'incoming_message',
-    NEW: 'new_message',
+    INCOMING: "incoming_message",
+    NEW: "new_message",
+  },
+  GAME: {
+    INCOMING: "incoming_game_requests",
+    ACCEPT: "accept_game_request",
+    DENY: "deny_game_request",
   },
 };
 
